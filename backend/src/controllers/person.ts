97d53b4fc7bcab52709feda299dfa2657c570e67
Richard Pánek <465758@mail.muni.cz>
2022-05-29 20:46:05 +0200

    Reformatting person

--- backend/src/controllers/person.ts
+++ backend/src/controllers/person.ts
@@ -1,156 +1,155 @@
-import { NextFunction, Request, Response } from 'express';
+import {NextFunction, Request, Response} from 'express';
+import {date, number, object, string, ValidationError} from 'yup';
+
 const {PrismaClient} = require('@prisma/client');
-import { object, string, number, date, ValidationError, boolean } from 'yup';
-import jwt from 'jsonwebtoken';
-import config from '../config/config';
 
 const prisma = new PrismaClient()
 
 const personList = async (req: Request, res: Response, next: NextFunction) => {
-    try{
+    try {
         const persons = await prisma.person.findMany({})
         res.json(persons)
-    } catch (error){
+    } catch (error) {
         next(error)
     }
 };
 
 const personSchema = object({
-   firstname: string(),
-   surename: string(),
-   degree: string(),
-   birthdate: date(),
-   email: string(),
-   phone: number(),
-   insuranceNumber: number(),
-   insuranceCode: number(),
-   insuranceName: string(),
-   country: string(),
-   city: string(),
-   postalCode: number(),
-   street: string(),
-   password: string(),
-   profilePicture: string(),
- });
+    firstname: string(),
+    surename: string(),
+    degree: string(),
+    birthdate: date(),
+    email: string(),
+    phone: number(),
+    insuranceNumber: number(),
+    insuranceCode: number(),
+    insuranceName: string(),
+    country: string(),
+    city: string(),
+    postalCode: number(),
+    street: string(),
+    password: string(),
+    profilePicture: string(),
+});
 
-const personDetail = async (req: Request, res: Response, next: NextFunction) => {
-  const person = await prisma.person.findMany({
-   where: {
-     email: res.locals.jwt.username
-   },
-   select: {
-       firstname: true,
-       surename: true,
-       degree: true,
-       email: true,
-       phone: true,
-       insuranceNumber: true,
-       insurance: {
-           select: {
-               number: true,
-               name: true,
-           }
-       },
-       address: {
-           select:{
-               country: true,
-               city: true,
-               postalCode: true,
-               street: true,
-           }
-       }
-   }
- });
-
- if (!person) {
-   return res.status(404).send({
-     status: "error",
-     data: {},
-     message: "Person was not found"
-   });
- }
-
- return res.send({
-   status: "sucess",
-   data: person[0],
- })
-}
-
-const personUpdate = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const data = await personSchema.validate(req.body);
-    const person = await prisma.person.updateMany({
-      where: {
-        email: res.locals.jwt.username
-      },
-      data: data
-    })
+const personDetail = async (req: Request, res: Response) => {
+    const person = await prisma.person.findMany({
+        where: {
+            email: res.locals.jwt.username
+        },
+        select: {
+            firstname: true,
+            surename: true,
+            degree: true,
+            email: true,
+            phone: true,
+            insuranceNumber: true,
+            insurance: {
+                select: {
+                    number: true,
+                    name: true,
+                }
+            },
+            address: {
+                select: {
+                    country: true,
+                    city: true,
+                    postalCode: true,
+                    street: true,
+                }
+            }
+        }
+    });
 
     if (!person) {
-      return res.status(404).send({
-        status: "error",
-        data: {},
-        message: "Person was not found"
-      });
+        return res.status(404).send({
+            status: "error",
+            data: {},
+            message: "Person was not found"
+        });
     }
 
     return res.send({
-      status: "sucess",
-      data: person
+        status: "sucess",
+        data: person[0],
     })
-  } catch (e) {
-    if (e instanceof ValidationError) {
-      return res.status(400).send({
-        status: "error",
-        data: e.errors,
-        message: e.message
-      });
-    }
-  }
 }
 
-const personReservations = async (req: Request, res: Response, next: NextFunction) => {
-  const reservations = await prisma.person.findMany({
-    where: {
-      email: res.locals.jwt.username
-    },
-    select: {
-        reservations: {
-            select: {
-              doctor: {
-                select: {
-                  person:{
-                    select: {
-                        firstname: true,
-                        surename: true,
-                        degree: true,
-                    }
-                },
-                specialization: true,
-                email: true,
-                }
-              },
-              from: true,
-              personComment: true,
-              doctorComment: true,
-              created: true
-            }
+const personUpdate = async (req: Request, res: Response) => {
+    try {
+        const data = await personSchema.validate(req.body);
+        const person = await prisma.person.updateMany({
+            where: {
+                email: res.locals.jwt.username
+            },
+            data: data
+        })
+
+        if (!person) {
+            return res.status(404).send({
+                status: "error",
+                data: {},
+                message: "Person was not found"
+            });
+        }
+
+        return res.send({
+            status: "sucess",
+            data: person
+        })
+    } catch (e) {
+        if (e instanceof ValidationError) {
+            return res.status(400).send({
+                status: "error",
+                data: e.errors,
+                message: e.message
+            });
         }
     }
-  });
-
-  if (!reservations) {
-    return res.status(404).send({
-      status: "error",
-      data: {},
-      message: "Person was not found"
-    });
-  }
- 
-  return res.send({
-    status: "sucess",
-    data: reservations[0],
-  })
 }
 
-export default { personList, personDetail, personUpdate, personReservations};
+const personReservations = async (req: Request, res: Response) => {
+    const reservations = await prisma.person.findMany({
+        where: {
+            email: res.locals.jwt.username
+        },
+        select: {
+            reservations: {
+                select: {
+                    doctor: {
+                        select: {
+                            person: {
+                                select: {
+                                    firstname: true,
+                                    surename: true,
+                                    degree: true,
+                                }
+                            },
+                            specialization: true,
+                            email: true,
+                        }
+                    },
+                    from: true,
+                    personComment: true,
+                    doctorComment: true,
+                    created: true
+                }
+            }
+        }
+    });
+
+    if (!reservations) {
+        return res.status(404).send({
+            status: "error",
+            data: {},
+            message: "Person was not found"
+        });
+    }
+
+    return res.send({
+        status: "sucess",
+        data: reservations[0],
+    })
+}
+
+export default {personList, personDetail, personUpdate, personReservations};
