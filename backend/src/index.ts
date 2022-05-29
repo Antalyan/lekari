16f39a9ddf61bb682dfc0e12815d905a15e23318
Richard Pánek <465758@mail.muni.cz>
2022-05-29 20:31:59 +0200

    Reformatting index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,14 @@
-//import { prisma, PrismaClient } from '@prisma/client';
 import auth from './controllers/auth';
 import person from './controllers/person';
 import doctor from './controllers/doctor';
 import express from 'express'
-const {PrismaClient} = require('@prisma/client');
-import { object, string, number, date, ValidationError, boolean } from 'yup';
+
 import extractJWT from './middleware/extractJWT';
 
-
 const api = express()
-const prisma = new PrismaClient()
 
 api.use(express.json());
-api.use(express.urlencoded({ extended: true }));
+api.use(express.urlencoded({extended: true}));
 
 api.get('/', (req, res) => res.send({
     status: "success",
@@ -24,10 +20,10 @@ api.get('/', (req, res) => res.send({
  * Resource person
  */
 
- api.get('/doctors', doctor.doctorList)
- api.get('/doctors/:id', doctor.doctorDetail)
- api.patch('/doctors/:id', doctor.doctorUpdate)
- api.get('/doctor-reservations', extractJWT, doctor.doctorReservations)
+api.get('/doctors', doctor.doctorList)
+api.get('/doctors/:id', doctor.doctorDetail)
+api.patch('/doctors/:id', doctor.doctorUpdate)
+api.get('/doctor-reservations', extractJWT, doctor.doctorReservations)
 
 
 api.get('/persons', person.personList)
@@ -39,6 +35,7 @@ api.get('/person-reservations', extractJWT, person.personReservations)
 api.get('/validate', extractJWT, auth.validateToken);
 api.post('/register', auth.register);
 api.post('/login', auth.login);
+
 /**
  * Start listening on connections
  */
