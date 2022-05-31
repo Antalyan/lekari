3cb4488999b43d98797192f39055f1cf90a75fde
Richard Pánek <465758@mail.muni.cz>
2022-05-31 15:32:48 +0200

    Rearranging index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,43 +1,42 @@
 import auth from './controllers/auth';
 import person from './controllers/person';
 import doctor from './controllers/doctor';
-import express from 'express'
 
 import extractJWT from './middleware/extractJWT';
 
-const api = express()
+const express = require('express');
+const api = express();
 
 api.use(express.json());
-api.use(express.urlencoded({extended: true}));
+api.use(express.urlencoded({ extended: true }));
 
-api.get('/', (req, res) => res.send({
-    status: "success",
-    data: {},
-    message: "Welcome to our API"
+api.get('/', (req: any, res: { send: (arg0: { status: string; data: {}; message: string; }) => any; }) => res.send({
+  status: 'success',
+  data: {},
+  message: 'Welcome to our API'
 }));
 
 /**
  * Resource person
  */
 
- api.get('/doctors', doctor.doctorList)
- api.get('/doctors/:id', doctor.doctorDetail)
- api.patch('/doctors/:id', doctor.doctorUpdate)
- api.get('/doctor-reservations', extractJWT, doctor.doctorReservations)
- api.get('/doctors/:id/slots/:date', doctor.doctorSlots)
+api.get('/doctors', doctor.doctorList);
+api.get('/doctors/:id', doctor.doctorDetail);
+api.patch('/doctors/:id', doctor.doctorUpdate);
+api.get('/doctor-reservations', extractJWT, doctor.doctorReservations);
+api.get('/doctors/:id/slots/:date', doctor.doctorSlots);
 
-
-api.get('/persons', person.personList)
-api.get('/personal-info', extractJWT, person.personDetail)
-api.patch('/personal-info', extractJWT, person.personUpdate)
-api.get('/person-reservations', extractJWT, person.personReservations)
-
+api.get('/persons', person.personList);
+api.get('/personal-info', extractJWT, person.personDetail);
+api.patch('/personal-info', extractJWT, person.personUpdate);
+api.get('/person-reservations', extractJWT, person.personReservations);
 
 api.get('/validate', extractJWT, auth.validateToken);
 api.post('/register', auth.register);
 api.post('/login', auth.login);
 api.put('/logout', auth.logout);
+
 /**
  * Start listening on connections
  */
-api.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}`))
+api.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}`));
