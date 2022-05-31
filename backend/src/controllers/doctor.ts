b8bf524147aff815c944a2f456859242639b8eb0
Richard Pánek <465758@mail.muni.cz>
2022-05-31 14:52:54 +0200

    Rearranging doctor controller

--- backend/src/controllers/doctor.ts
+++ backend/src/controllers/doctor.ts
@@ -1,11 +1,8 @@
-import { NextFunction, Request, Response } from 'express';
-const {Prisma, PrismaClient} = require('@prisma/client');
-//import { Prisma, PrismaClient } from '@prisma/client'
-import { object, string, number, date, ValidationError, boolean } from 'yup';
+import { Request, Response } from 'express';
+import prisma from '../client';
+import { object, string, ValidationError } from 'yup';
 
-const prisma = new PrismaClient()
-
-const doctorDetail = async (req: Request, res: Response, next: NextFunction) => {
+const doctorDetail = async (req: Request, res: Response) => {
   const id = req.params.id;
 
   const doctor = await prisma.doctor.findUnique({
@@ -13,221 +10,229 @@ const doctorDetail = async (req: Request, res: Response, next: NextFunction) => 
       id: id
     },
     select: {
-        person:{
-            select: {
-                firstname: true,
-                surename: true,
-                degree: true,
-            }
-        },
-        specialization: true,
-        email: true,
-        phone: true,
-        description: true,
-        link: true,
-        languages: {
-            select:{
-                language: true
-            }
-        },
-        address: {
-            select: {
-                country: true,
-                city: true,
-                postalCode: true,
-                street: true,
-            }
-        },
-        profilePicture: true,
-        actuality: true,
-        openingHours: {
-            select: {
-                day: true,
-                fromTime: true,
-                toTime: true,
-                fromDate: true,
-            }
-        },
-        references: {
-            select: {
-                rate: true,
-                comment: true,
-                author: true,
-                created: true,
-            }
+      person: {
+        select: {
+          firstname: true,
+          surename: true,
+          degree: true,
         }
+      },
+      specialization: true,
+      email: true,
+      phone: true,
+      description: true,
+      link: true,
+      languages: {
+        select: {
+          language: true
+        }
+      },
+      address: {
+        select: {
+          country: true,
+          city: true,
+          postalCode: true,
+          street: true,
+        }
+      },
+      profilePicture: true,
+      actuality: true,
+      openingHours: {
+        select: {
+          day: true,
+          fromTime: true,
+          toTime: true,
+          fromDate: true,
+        }
+      },
+      references: {
+        select: {
+          rate: true,
+          comment: true,
+          author: true,
+          created: true,
+        }
+      }
     }
   });
 
   if (!doctor) {
-    return res.status(404).send({
-      status: "error",
-      data: {},
-      message: "Person was not found"
-    });
+    return res.status(404)
+      .send({
+        status: 'error',
+        data: {},
+        message: 'Person was not found'
+      });
   }
 
   return res.send({
-    status: "sucess",
+    status: 'success',
     data: doctor
-  })
-}
+  });
+};
 
-const doctorList = async (req: Request, res: Response, next: NextFunction) => {
-  const { surename, location, specialization, orderBy } = req.query
+const doctorList = async (req: Request, res: Response) => {
+  const {
+    surename,
+    location,
+    specialization
+  } = req.query;
 
   const doctors = await prisma.doctor.findMany({
     where: {
-      person:{
+      person: {
         surename: {
-          contains: surename != null ? surename : undefined
+          contains: surename as string || undefined
         },
       },
       specialization: {
-        contains: specialization != null ? specialization: undefined
+        contains: specialization as string || undefined
       },
       address: {
         city: {
-          contains: location != null ? location: undefined
+          contains: location as string || undefined
         },
       }
     },
     select: {
-        id: true,
-        person: {
-            select: {
-                firstname: true,
-                surename: true,
-            }
-        },
-        specialization: true,
-        address:{
-            select: {
-                city: true,
-            }
-        },
-        actuality: true,
+      id: true,
+      person: {
+        select: {
+          firstname: true,
+          surename: true,
+        }
       },
+      specialization: true,
+      address: {
+        select: {
+          city: true,
+        }
+      },
+      actuality: true,
+    },
   });
 
   if (!doctors) {
-    return res.status(404).send({
-      status: "error",
-      data: {},
-      message: "Person was not found"
-    });
+    return res.status(404)
+      .send({
+        status: 'error',
+        data: {},
+        message: 'Person was not found'
+      });
   }
 
   return res.send({
-    status: "sucess",
+    status: 'sucess',
     data: doctors
-  })
-}
+  });
+};
 
 const doctorSchema = object({
-  specialization: string().matches(/(chirirg|psychytr|ortoped)/),
-  actuality:      string(),
-  email:          string(),
-  phone:          string(),
-  description:    string(),
-  link:           string(),
+  specialization: string()
+    .matches(/(chirirg|psychytr|ortoped)/),
+  actuality: string(),
+  email: string(),
+  phone: string(),
+  description: string(),
+  link: string(),
   profilePicture: string(),
-  addressId:      string()
+  addressId: string()
 });
 
-const doctorUpdate = async (req: Request, res: Response, next: NextFunction) => {
+const doctorUpdate = async (req: Request, res: Response) => {
   const id = req.params.id;
   try {
     const data = await doctorSchema.validate(req.body);
     const doctor = await prisma.doctor.updateMany({
       where: {
-          id: id
+        id: id
       },
       data: data
-    })
+    });
 
     if (!doctor) {
-      return res.status(404).send({
-        status: "error",
-        data: {},
-        message: "Person was not found"
-      });
+      return res.status(404)
+        .send({
+          status: 'error',
+          data: {},
+          message: 'Person was not found'
+        });
     }
 
     return res.send({
-      status: "sucess",
+      status: 'sucess',
       data: doctor
-    })
+    });
   } catch (e) {
     if (e instanceof ValidationError) {
-      return res.status(400).send({
-        status: "error",
-        data: e.errors,
-        message: e.message
-      });
+      return res.status(400)
+        .send({
+          status: 'error',
+          data: e.errors,
+          message: e.message
+        });
     }
   }
-}
+};
 
-const doctorReservations = async (req: Request, res: Response, next: NextFunction) => {
+const doctorReservations = async (req: Request, res: Response) => {
   const reservations = await prisma.doctor.findMany({
     where: {
-      person:{
-          email: res.locals.jwt.username
+      person: {
+        email: res.locals.jwt.username
       }
     },
     select: {
-        reservations: {
+      reservations: {
+        select: {
+          person: {
             select: {
-              person: {
-                select: {
-                    firstname: true,
-                    surename: true,
-                    degree: true,
-                    email: true,
-                    phone: true
-                }
-              },
-              from: true,
-              personComment: true,
-              doctorComment: true,
-              created: true
+              firstname: true,
+              surename: true,
+              degree: true,
+              email: true,
+              phone: true
             }
+          },
+          from: true,
+          personComment: true,
+          created: true
         }
+      }
     }
   });
 
   if (!reservations) {
-    return res.status(404).send({
-      status: "error",
-      data: {},
-      message: "Person was not found"
-    });
+    return res.status(404)
+      .send({
+        status: 'error',
+        data: {},
+        message: 'Person was not found'
+      });
   }
- 
-  return res.send({
-    status: "sucess",
-    data: reservations[0],
-  })
-}
 
-const doctorSlots = async (req: Request, res: Response, next: NextFunction) => {
+  return res.send({
+    status: 'sucess',
+    data: reservations[0],
+  });
+};
+
+const doctorSlots = async (req: Request, res: Response) => {
   const id = req.params.id;
   const date = new Date(req.params.date);
-  var tomorrow = new Date();
-  tomorrow.setDate(date.getDate()+1);
-  var weekday=new Array(7);
-  weekday[0]="Monday";
-  weekday[1]="Tuesday";
-  weekday[2]="Wednesday";
-  weekday[3]="Thursday";
-  weekday[4]="Friday";
-  weekday[5]="Saturday";
-  weekday[6]="Sunday";
+  const tomorrow = new Date();
+  tomorrow.setDate(date.getDate() + 1);
+  const weekday = new Array(7);
+  weekday[0] = 'Monday';
+  weekday[1] = 'Tuesday';
+  weekday[2] = 'Wednesday';
+  weekday[3] = 'Thursday';
+  weekday[4] = 'Friday';
+  weekday[5] = 'Saturday';
+  weekday[6] = 'Sunday';
   const day = weekday[date.getDay()];
-  
 
-  const openingHours = await prisma.openingHours.findMany({
+  const openingHours = await prisma.reservationHours.findMany({
     where: {
       doctorId: id,
       day: day,
@@ -236,7 +241,7 @@ const doctorSlots = async (req: Request, res: Response, next: NextFunction) => {
       fromTime: true,
       toTime: true,
       fromDate: true,
-      interval:true
+      interval: true
     }
   });
 
@@ -254,31 +259,38 @@ const doctorSlots = async (req: Request, res: Response, next: NextFunction) => {
   });
 
   if (!openingHours) {
-    return res.status(404).send({
-      status: "error",
-      data: {},
-      message: "Opening hours was not found"
-    });
+    return res.status(404)
+      .send({
+        status: 'error',
+        data: {},
+        message: 'Opening hours was not found'
+      });
   }
-  let dateFrom = new Date(date)
-  dateFrom.setTime(openingHours[0].fromTime.getTime())
-  let allTimeSlots = []
+  let dateFrom = new Date(date);
+  dateFrom.setTime(openingHours[0].fromTime.getTime());
+  let allTimeSlots = [];
   let time = openingHours[0].toTime.getTime() - openingHours[0].fromTime.getTime();
-  let lastTime = new Date (dateFrom);
+  let lastTime = new Date(dateFrom);
   for (let i = 0; i < (((time / 60) / openingHours[0].interval) / 1000); i++) {
-    allTimeSlots.push(new Date(lastTime))
-    lastTime.setMinutes( lastTime.getMinutes() + 30 )
+    allTimeSlots.push(new Date(lastTime));
+    lastTime.setMinutes(lastTime.getMinutes() + 30);
   }
 
-  const reservationsTimes = reservations.map((item: { from: any; }) => item.from.toLocaleString())
-  let timeSlots = allTimeSlots.filter( function( el ) {
+  const reservationsTimes = reservations.map((item: { from: any; }) => item.from.toLocaleString());
+  let timeSlots = allTimeSlots.filter(function (el) {
     return !reservationsTimes.includes(el.toLocaleString());
-  } );
+  });
 
   return res.send({
-    status: "sucess",
+    status: 'sucess',
     data: timeSlots
-  })
-}
+  });
+};
 
-export default { doctorList, doctorDetail, doctorUpdate, doctorReservations, doctorSlots};
+export default {
+  doctorList,
+  doctorDetail,
+  doctorUpdate,
+  doctorReservations,
+  doctorSlots
+};
