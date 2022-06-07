f58fe32b0c000d47daaa77e5e44693b364022ee1
Richard Pánek <465758@mail.muni.cz>
2022-06-07 21:06:56 +0200

    Introducing doctorModel

---
+++ backend/src/models/doctorModel.ts
@@ -0,0 +1,26 @@
+import prisma from '../client';
+
+const getDoctorFromPerson = async (personId: number) => {
+  return await prisma.person.findFirst({
+    where: {
+      deleted: false,
+      id: personId,
+    },
+    include: {
+      doctor: {
+        include: {
+          address: true,
+          languages: true,
+          openingHours: true,
+          references: {
+            orderBy: {
+              created: 'desc'
+            }
+          }
+        }
+      },
+    },
+  });
+};
+
+export default getDoctorFromPerson;
