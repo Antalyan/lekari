a3a36c21a816bf7c00f6d1b7e3eb6b69ba4ce810
Richard Pánek <465758@mail.muni.cz>
2022-06-09 12:15:59 +0200

    Setting token expiration to 1 day

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const SERVER_TOKEN_EXPIRATION = process.env.SERVER_TOKEN_EXPIRETIME || '10m';
+const SERVER_TOKEN_EXPIRATION = process.env.SERVER_TOKEN_EXPIRETIME || '1d';
 const SERVER_TOKEN_ISSUER = process.env.SERVER_TOKEN_ISSUER || 'coolIssuer';
 const SERVER_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || 'superencryptedsecret';
 
