b727d4da35a40a943859d9c3ba923371ee46a5e7
Richard Pánek <465758@mail.muni.cz>
2022-06-09 03:10:42 +0200

    Rearranging helperFunctions

--- backend/src/controllers/helperFunctions.ts
+++ backend/src/controllers/helperFunctions.ts
@@ -1,29 +1,28 @@
-
-
 export const createDatetime = (date: Date, time: string) => {
-    let splitedTime = time.split(':')
-    if(splitedTime.length === 2){
-      let datetime = new Date(date)
-      datetime.setHours(parseInt(splitedTime[0]))
-      datetime.setMinutes(parseInt(splitedTime[1]))
-      return datetime
-    }
-    return null
-}
+  let splitedTime = time.split(':');
+  if (splitedTime.length === 2) {
+    let datetime = new Date(date);
+    datetime.setHours(parseInt(splitedTime[0]));
+    datetime.setMinutes(parseInt(splitedTime[1]));
+    return datetime;
+  }
+  return null;
+};
 
 export const convertTimeToString = (datetime: Date) => {
-    if(datetime){
-      let splitTime = datetime.toTimeString().match(/([0-9]+:[0-9]+)/g);
-      if(splitTime){
-        return splitTime[0]
-      }
+  if (datetime) {
+    let splitTime = datetime.toTimeString()
+      .match(/([0-9]+:[0-9]+)/g);
+    if (splitTime) {
+      return splitTime[0];
     }
-    return null
-}
+  }
+  return null;
+};
 
-export const getTimeInMinutes = (datetime: Date|null) => {
-    if(datetime){
-      return datetime.getHours() * 60 + datetime.getMinutes() 
-    }
-    return null
-}
+export const getTimeInMinutes = (datetime: Date | null) => {
+  if (datetime) {
+    return datetime.getHours() * 60 + datetime.getMinutes();
+  }
+  return null;
+};
