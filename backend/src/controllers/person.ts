1a4a1febe16ab6425a7a92e584fe5f910f4cf253
Richard Pánek <465758@mail.muni.cz>
2022-06-10 04:29:22 +0200

    Introducing personAdapter

--- backend/src/controllers/person.ts
+++ backend/src/controllers/person.ts
@@ -3,30 +3,14 @@ import personSchema from './schemas/personSchema';
 import results from '../utilities/results';
 import personModel from '../models/personModel';
 import updateUtils from '../utilities/updateUtils';
+import personAdapter from '../dataAdapters/personAdapter';
 
 const detail = async (req: Request, res: Response) => {
-  
+
   const person = res.locals.jwt;
   if (!person || person.deleted) return results.error(res, 'Person was not found.', 404);
 
-  return res.send({
-    status: 'success',
-    data: {
-      firstname: person.firstname,
-      surname: person.surname,
-      degree: person.degree || null,
-      email: person.email,
-      phone: person.phone,
-      phonePrefix: person.phonePrefix,
-      birthdate: person.birthdate,
-      insuranceNumber: person.insuranceNumber || null,
-      country: person.address.country,
-      city: person.address.city,
-      postalCode: person.address.postalCode,
-      street: person.address.street || null,
-      buildingNumber: person.address.buildingNumber,
-    }
-  });
+  return results.success(res, personAdapter.detail(person), 200);
 };
 
 const update = async (req: Request, res: Response) => {
